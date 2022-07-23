cd4cea2872695ed8b1b1de2272cafe0766e99529
Simon Jenny <simon.jenny@me.com>
2022-07-23 13:09:04 +0200

    Locale Opt

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import apps from './components/apps.vue'
 import greetings from './components/greetings.vue'
 import bookmarks from './components/bookmarks.vue'
 import countdown from './components/countdown.vue'
-import news from './components/news.vue'
 </script>
 <template>
     <main class="fade p-4 sm:p-8 md:p-18 lg:p-28">
--- src/components/apps.vue
+++ src/components/apps.vue
@@ -10,11 +10,11 @@ export default {
 <template>
     <h2 class="my-6 font-semibold text-xl text-gray-100 leading-tight">Applications</h2>
     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <a :href="app.url" target="_blank" v-for="app in apps" class="flex items-center relative p-4 w-full bg-gray-100 rounded overflow-hidden shadow border-gray-400 border-l-8 hover:no-underline">
-            <Icon :icon="'mdi-' + app.icon" class="w-12 h-12 p-2 rounded bg-gray-700 text-gray-200" />
+        <a :href="app.url" target="_blank" v-for="app in apps" class="flex items-center relative p-4 w-full bg-gray-800 text-gray-200 rounded overflow-hidden shadow border-gray-600 border-l-8 hover:bg-gray-600 hover:no-underline hover:text">
+            <Icon :icon="'mdi-' + app.icon" class="w-12 h-12 p-2 rounded bg-gray-700 " />
             <div class="ml-3">
-            <p class="text-lg font-medium text-gray-800">{{ app.name }}</p>
-            <p class="text-xs text-gray-600">{{ app.url }}</p>
+                <p class="text-lg font-medium">{{ app.name }}</p>
+                <p class="text-xs text-gray-400">{{ app.url }}</p>
             </div>
         </a>
     </div>
